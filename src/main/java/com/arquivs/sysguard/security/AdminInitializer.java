ea5b342ced3cdb6f043b058d3efa5e45f78f8da5
David <daviddavi1221@gmail.com>
2025-05-08 15:04:03 -0300

    Adicionando criaçao de admin assim que servidor estarta

--- src/main/java/com/arquivs/sysguard/security/AdminInitializer.java
+++ src/main/java/com/arquivs/sysguard/security/AdminInitializer.java
@@ -4,6 +4,7 @@ import com.arquivs.sysguard.entity.UserEntity;
 import com.arquivs.sysguard.model.Role;
 import com.arquivs.sysguard.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -11,16 +12,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class AdminInitializer implements CommandLineRunner {
 
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Value("${ADMIN_LOGIN}")
+    private String adminLogin;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         if (usuarioRepository.count() == 0) {
             UserEntity admin = new UserEntity();
-            admin.setLogin("${ADMIN_LOGIN}");
-            admin.setPassword(new BCryptPasswordEncoder().encode("${ADMIN_PASSWORD}"));
-            admin.setRole(Role.valueOf("ADMIN"));
+            admin.setLogin(adminLogin);
+            admin.setPassword(new BCryptPasswordEncoder().encode(adminPassword));
+            admin.setRole(Role.ADMIN);
             usuarioRepository.save(admin);
             System.out.println("Admin criado com sucesso.");
         }
