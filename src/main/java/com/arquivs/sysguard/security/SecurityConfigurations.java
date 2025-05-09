08901c85bd00e9d3091135eef7f274d3c57f43a2
David <daviddavi1221@gmail.com>
2025-05-08 22:12:40 -0300

    Permitindo conexão local de front para testes

--- src/main/java/com/arquivs/sysguard/security/SecurityConfigurations.java
+++ src/main/java/com/arquivs/sysguard/security/SecurityConfigurations.java
@@ -30,7 +30,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/").permitAll()
                         .requestMatchers(HttpMethod.POST,"/locatarios").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
